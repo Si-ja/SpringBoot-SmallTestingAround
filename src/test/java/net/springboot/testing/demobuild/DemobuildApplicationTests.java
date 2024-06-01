14ffcf154c6ca298be0a8d646caa5207070bdf07
Nikita <nicksemion@gmail.com>
2024-06-01 12:44:59 +0300

    Initial Commit
    Initial Commit

---
+++ src/test/java/net/springboot/testing/demobuild/DemobuildApplicationTests.java
@@ -0,0 +1,13 @@
+package net.springboot.testing.demobuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
